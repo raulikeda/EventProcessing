d3bf990db6a3d5a3b351531a66fbd00a2d592b83
raulikeda <rauligs@insper.edu.br>
2024-02-03 13:57:39 -0300

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="event-processing",
-    version="0.0.8",
+    version="0.0.9",
     author="Raul Ikeda",
     description="Simple Event Processing Library",
     long_description=long_description,
